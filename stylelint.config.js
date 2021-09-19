c91757c6bd05e46e653708ad6daec57002d32a3b
Hassan Ali <hassan.ali@empired.com>
2021-09-19 18:06:27 +1000

    switch to scss syntax

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,8 @@ module.exports = {
   plugins: ["stylelint-order", "stylelint-scss"],
   rules: {
     "order/properties-alphabetical-order": true,
+    "scss/at-rule-no-unknown": null,
+    "scss/at-import-no-partial-leading-underscore": null,
     "at-rule-no-unknown": [
       true,
       {
